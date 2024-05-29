a57b18c0877be4fc2ed8370cf3105a8c42913b4a
furkanakcay <fakcay@uplandsoftware.com>
2024-05-29 09:10:13 +0300

    remote unnecessary comment

--- src/main/java/com/akcay/dashboard/configuration/WebConfig.java
+++ src/main/java/com/akcay/dashboard/configuration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping(ControllerConstants.API_PREFIX + "/**")  // Adjust the mapping to match your API endpoints
+        registry.addMapping(ControllerConstants.API_PREFIX + "/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
